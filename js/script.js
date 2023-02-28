d5bf147ca5b621294eae3fd9fc394fe6f0d2fafc
EnricoDErasmo <enrico.derasmo@gmail.com>
2023-02-28 22:42:22 +0100

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*
 Consegna:
 Dato un array contenente una lista di cinque immagini,
- creare un carosello come nello screenshot allegato.
+creare un carosello come nello screenshot allegato.
 
 MILESTONE 1
 Per prima cosa, creiamo il markup statico:
@@ -22,4 +22,20 @@ MILESTONE 3
 Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva,
 che quindi verrà visualizzata al posto della precedente.
 */
+
+
+
+// Creo un array di immagini
+const images = ["img/01.webp", "img/02.webp", "img/03.webp" , "img/04.webp" , "img/05.webp"];
+
+// Creo variabili per bersagliare elementi html
+const upArrowEl = document.getElementById("upArrow");
+const downArrowEl = document.getElementById("downArrow");
+const activeImgEl = document.getElementById("carouselActiveImg");
+
+// Creo una variabile di indice
+let index = 0;
+
+// Faccio in modo che all'apertura della pagina web sia visualizzata la prima immagine dell'array
+activeImgEl.src = images[index];
 
